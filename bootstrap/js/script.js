20b7bbb735e81d17646adeb746d1990f9074db0a
cmabdullah <a.kium.khan@gmail.com>
2016-09-08 17:11:23 -0700

    js addition , sub , equality , strict , equality l143-2

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -20,6 +20,8 @@ b();
 
 a();
 */
+
+/*
 var x;
 console.log(x);
 if (x == undefined){
@@ -33,5 +35,40 @@ if (x == undefined) {
 else{
 	console.log("x has been defined");
 }
+*/
 
+/*
+var string = "hello";
+// string += " World";
+string = string+ " World";
+console.log(string + "!");
+*/
+/**math operation***/
+
+// console.log((10+2)/6);
+// console.log(undefined/90);
+
+// function cm (c){
+// 	console.log(c / 10);
+// }
+// cm();
+
+/*****************equality*/
+var x =4, y=4;
+if (x == y){
+	console.log("x=4 is equal to y=4");
+}
+x = "4";
+if (x == y){
+	console.log("x='4' is equal to y = 4");
+}
+
+
+/*********strict equality***/
+if (x === y){
+	console.log("strict x='4' is equal to y=4") ;
+}
+else{
+	console.log("strict x='4' is not equal to y=4");
+}
 
