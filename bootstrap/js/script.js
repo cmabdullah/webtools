a4db43e78b6e6bbd1c5596cca322ca86da8fd697
cmabdullah <a.kium.khan@gmail.com>
2016-09-08 17:47:23 -0700

    for loop l1143-3

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -54,21 +54,42 @@ console.log(string + "!");
 // cm();
 
 /*****************equality*/
-var x =4, y=4;
-if (x == y){
-	console.log("x=4 is equal to y=4");
-}
-x = "4";
-if (x == y){
-	console.log("x='4' is equal to y = 4");
-}
+// var x =4, y=4;
+// if (x == y){
+// 	console.log("x=4 is equal to y=4");
+// }
+// x = "4";
+// if (x == y){
+// 	console.log("x='4' is equal to y = 4");
+// }
 
 
-/*********strict equality***/
-if (x === y){
-	console.log("strict x='4' is equal to y=4") ;
-}
-else{
-	console.log("strict x='4' is not equal to y=4");
-}
+// ********strict equality**
+// if (x === y){
+// 	console.log("strict x='4' is equal to y=4") ;
+// }
+// else{
+// 	console.log("strict x='4' is not equal to y=4");
+// }
 
+
+/**************boolian expression true or false***/
+
+// if (false || null || undefined || "" || 0 || NaN){
+// 	console.log("this line wont be able to execute");
+// }
+// else{
+// 	console.log("All are false");
+// }
+// /********************true statement********/
+// if (true && "hello" && 1 && -1 && "false") {
+// 	console.log("All are true");
+// }
+
+// For loop
+var sum = 0;
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+  sum = sum + i;
+}
+console.log("sum of 0 through 9 is: " + sum);
